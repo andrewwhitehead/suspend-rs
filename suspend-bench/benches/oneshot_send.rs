c1df68a24e4277d9caaa81bfa65c6cf98885ed15
Andrew Whitehead <cywolf@gmail.com>
2020-12-21 19:34:59 -0800

    copy benches
    Signed-off-by: Andrew Whitehead <cywolf@gmail.com>

---
+++ suspend-bench/benches/oneshot_send.rs
@@ -0,0 +1,50 @@
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
+
+use futures_channel::oneshot as futures_oneshot;
+use futures_lite::future::block_on;
+use oneshot_rs as oneshot;
+use suspend_channel::send_once;
+
+fn send_once_many(count: usize) {
+    for _ in 0..count {
+        let (sender, receiver) = send_once();
+        sender.send(1).unwrap();
+        block_on(receiver).unwrap();
+    }
+}
+
+fn futures_many(count: usize) {
+    for _ in 0..count {
+        let (sender, receiver) = futures_oneshot::channel();
+        sender.send(1).unwrap();
+        block_on(receiver).unwrap();
+    }
+}
+
+fn oneshot_many(count: usize) {
+    for _ in 0..count {
+        let (sender, receiver) = oneshot::channel();
+        sender.send(1).unwrap();
+        block_on(receiver).unwrap();
+    }
+}
+
+fn bench_many(c: &mut Criterion) {
+    let count = 5000;
+    c.bench_with_input(
+        BenchmarkId::new("send_once-many", count),
+        &count,
+        |b, &s| {
+            b.iter(|| send_once_many(s));
+        },
+    );
+    c.bench_with_input(BenchmarkId::new("futures-many", count), &count, |b, &s| {
+        b.iter(|| futures_many(s));
+    });
+    c.bench_with_input(BenchmarkId::new("oneshot-many", count), &count, |b, &s| {
+        b.iter(|| oneshot_many(s));
+    });
+}
+
+criterion_group!(benches, bench_many);
+criterion_main!(benches);
---
+++ suspend-bench/benches/oneshot_thread.rs
@@ -0,0 +1,81 @@
+use std::thread;
+
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
+
+use futures_channel::oneshot as futures_oneshot;
+use futures_lite::future::block_on;
+use oneshot_rs as oneshot;
+use suspend_channel::send_once;
+
+fn send_once_telephone(threads: usize) {
+    let (sender, mut receiver) = send_once();
+    for _ in 0..threads {
+        let (next_send, next_receive) = send_once();
+        thread::spawn(move || {
+            let result = block_on(receiver).unwrap_or(0);
+            next_send.send(result).unwrap();
+        });
+        receiver = next_receive;
+    }
+    let testval = 1001u32;
+    sender.send(testval).unwrap();
+    assert_eq!(block_on(receiver), Ok(testval));
+}
+
+fn futures_telephone(threads: usize) {
+    let (sender, mut receiver) = futures_oneshot::channel();
+    for _ in 0..threads {
+        let (next_send, next_receive) = futures_oneshot::channel();
+        thread::spawn(move || {
+            let result = block_on(receiver).unwrap_or(0);
+            next_send.send(result).unwrap();
+        });
+        receiver = next_receive;
+    }
+    let testval = 1001u32;
+    sender.send(testval).unwrap();
+    assert_eq!(block_on(receiver), Ok(testval));
+}
+
+fn oneshot_telephone(threads: usize) {
+    let (sender, mut receiver) = oneshot::channel();
+    for _ in 0..threads {
+        let (next_send, next_receive) = oneshot::channel();
+        thread::spawn(move || {
+            let result = block_on(receiver).unwrap_or(0);
+            next_send.send(result).unwrap();
+        });
+        receiver = next_receive;
+    }
+    let testval = 1001u32;
+    sender.send(testval).unwrap();
+    assert_eq!(receiver.recv(), Ok(testval));
+}
+
+fn bench_telephone(c: &mut Criterion) {
+    let count = 1000;
+    c.bench_with_input(
+        BenchmarkId::new("message-telephone", count),
+        &count,
+        |b, &s| {
+            b.iter(|| send_once_telephone(s));
+        },
+    );
+    c.bench_with_input(
+        BenchmarkId::new("futures-telephone", count),
+        &count,
+        |b, &s| {
+            b.iter(|| futures_telephone(s));
+        },
+    );
+    c.bench_with_input(
+        BenchmarkId::new("oneshot-telephone", count),
+        &count,
+        |b, &s| {
+            b.iter(|| oneshot_telephone(s));
+        },
+    );
+}
+
+criterion_group!(benches, bench_telephone);
+criterion_main!(benches);
