77c704106d6fa186a63bbfa778b4ef442e084f7d
Andrew Whitehead <cywolf@gmail.com>
2021-01-07 16:33:44 -0800

    minor cleanups
    Signed-off-by: Andrew Whitehead <cywolf@gmail.com>

--- suspend-bench/benches/threadpool.rs
+++ suspend-bench/benches/threadpool.rs
@@ -3,7 +3,7 @@ use criterion::{criterion_group, criterion_main, Criterion};
 use blocking;
 use once_cell::sync::Lazy;
 use suspend_core::listen::block_on;
-use suspend_exec::ThreadPool;
+use suspend_exec::{ThreadPool, ThreadPoolConfig};
 
 #[cfg(target = "macos")]
 #[global_allocator]
